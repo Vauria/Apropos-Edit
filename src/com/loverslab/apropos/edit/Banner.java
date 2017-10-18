52aa8a2ee92b7f56ed82fb17712988cdefa3e10b
Vauria <vauriae@gmail.com>
2017-10-18 15:01:52 +0100

    Fixed that issue, and several other outstanding bugs around that filechooser.

--- src/com/loverslab/apropos/edit/Banner.java
+++ src/com/loverslab/apropos/edit/Banner.java
@@ -23,6 +23,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.LookAndFeel;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.EventListenerList;
@@ -88,10 +89,9 @@ public class Banner extends JPanel implements ItemListener, ActionListener {
 				String selection = (String) model.getSelectedItem();
 				if ( !selection.equals( "<other>" ) ) {
 					locations.removeItemAt( locations.getSelectedIndex() );
-					System.out.println( selection );
 					Globals globals = Banner.this.parent.globals;
 					String gLocations = globals.getProperty( "locations" );
-					System.out.println( gLocations );
+					// Rebuild the locations string excluding this entry
 					String[] locationsArr = gLocations.split( Pattern.quote( globals.delimiter ) );
 					String newLocations = "";
 					int found = -1;
@@ -104,8 +104,6 @@ public class Banner extends JPanel implements ItemListener, ActionListener {
 					}
 					// Trim the last delimiter
 					newLocations = newLocations.replaceAll( Pattern.quote( globals.delimiter ) + "$", "" );
-					System.out.println( found );
-					System.out.println( newLocations );
 					globals.setProperty( "locations", newLocations );
 				}
 			}
@@ -148,6 +146,8 @@ public class Banner extends JPanel implements ItemListener, ActionListener {
 	 * For when there is only one item, the <other> item, no new item can be selected, and so no new items can be added
 	 */
 	public void actionPerformed( ActionEvent e ) {
+		if ( locations.getItemCount() != 1 ) return; // This method needs to be prevented from firing if the itemStateChange listener is
+													 // active
 		String selected = (String) model.getSelectedItem();
 		if ( selected.equals( "<other>" ) ) {
 			itemStateChanged( new ItemEvent( locations, ItemEvent.ITEM_STATE_CHANGED, selected, ItemEvent.SELECTED ) );
@@ -158,38 +158,53 @@ public class Banner extends JPanel implements ItemListener, ActionListener {
 		if ( e.getStateChange() == ItemEvent.SELECTED ) {
 			String selected = (String) model.getSelectedItem();
 			if ( selected.equals( "<other>" ) ) {
-				JSystemFileChooser fileChooser = new JSystemFileChooser();
-				fileChooser.setAcceptAllFileFilterUsed( false );
-				fileChooser.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
-				try { // Set the default directory to the one the code was launched from
-					fileChooser.setCurrentDirectory( new File( ClassLoader.getSystemClassLoader().getResource( "." ).toURI().getPath() ) );
-				}
-				catch ( URISyntaxException ex ) {
-					parent.handleException( ex );
-					ex.printStackTrace();
-				}
-				int result = fileChooser.showOpenDialog( this );
-				
-				if ( result == JFileChooser.APPROVE_OPTION ) {
-					File chosenFile = fileChooser.getSelectedFile();
-					// Update locations in config file
-					String oldLoc = parent.globals.getProperty( "locations" );
-					parent.globals.setProperty( "locations",
-							oldLoc + ( ( oldLoc.split( parent.globals.delimiter ).length > 0 & !oldLoc.equals( "" ) )
-									? parent.globals.delimiter : "" ) + chosenFile.getAbsolutePath() );
-					locations.insertItemAt( chosenFile.getAbsolutePath(), locations.getItemCount() - 1 );
-					locations.setSelectedIndex( locations.getItemCount() - 2 );
-					// Above call will trigger another itemStateChange event, which we will rely on to trigger the Database Change
-					return;
-				}
-				else {
-					locations.setSelectedIndex( 0 );
-					return;
-				}
-				
+				SwingUtilities.invokeLater( new Runnable() {
+					public void run() {
+						JSystemFileChooser fileChooser = new JSystemFileChooser();
+						fileChooser.setAcceptAllFileFilterUsed( false );
+						fileChooser.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
+						try { // Set the default directory to the one the code was launched from
+							String spoofLoc = parent.globals.getProperty( "spoofloc" );
+							if ( spoofLoc != null )
+								fileChooser.setCurrentDirectory( new File( spoofLoc ) );
+							else
+								fileChooser.setCurrentDirectory(
+										new File( ClassLoader.getSystemClassLoader().getResource( "." ).toURI().getPath() ) );
+						}
+						catch ( URISyntaxException ex ) {
+							parent.handleException( ex );
+							ex.printStackTrace();
+						}
+						int result = fileChooser.showOpenDialog( Banner.this );
+						
+						if ( result == JFileChooser.APPROVE_OPTION ) {
+							File chosenFile = fileChooser.getSelectedFile();
+							// Update locations in config file
+							String oldLoc = parent.globals.getProperty( "locations" );
+							parent.globals.setProperty( "locations",
+									oldLoc + ( ( oldLoc.split( parent.globals.delimiter ).length > 0 & !oldLoc.equals( "" ) )
+											? parent.globals.delimiter : "" ) + chosenFile.getAbsolutePath() );
+							locations.insertItemAt( chosenFile.getAbsolutePath(), locations.getItemCount() - 1 );
+							locations.setSelectedIndex( locations.getItemCount() - 2 );
+							// Above call will trigger another itemStateChange event, which we will rely on to trigger the Database Change
+							return;
+						}
+						else {
+							// Check there's actually another element to check, otherwise we'll just reopen the FileChooser.
+							// Again and again.
+							// Not that that isn't hilarious of course. PLEASE CHOSE A DIRECTORY I BEG YOU
+							if ( locations.getItemCount() > 1 ) locations.setSelectedIndex( 0 );
+							return;
+						}
+					}
+				} );
+				// The FileChooser dialog blocks the running thread until the user makes their input. But this is an event, and thus we're
+				// on the EDT. Suspected cause of fuckery.
 			}
-			String newSelection = (String) model.getSelectedItem();
-			fireActionPerformed( newSelection );
+			else {
+				String newSelection = (String) model.getSelectedItem();
+				fireActionPerformed( newSelection );
+			}
 		}
 	}
 	
